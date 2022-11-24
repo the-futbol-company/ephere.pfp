4ff1eaf64f27b106a6a38cc0c230444a7ec01d57
Agu Rodríguez <me@agurodriguez.net>
2022-11-24 18:29:41 -0300

    Fix URL

--- src/main/kotlin/io/ephere/pfp/controller/EphereProfilePicOnPolygonController.kt
+++ src/main/kotlin/io/ephere/pfp/controller/EphereProfilePicOnPolygonController.kt
@@ -1,21 +1,21 @@
 package io.ephere.pfp.controller
 
 import io.ephere.pfp.model.ERC721Metadata
-import io.ephere.pfp.service.EphereProfilePicService
+import io.ephere.pfp.service.EphereProfilePicOnPolygonService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/erc721")
-class EphereProfilePicController(
-  private val ephereProfilePicService: EphereProfilePicService
+@RequestMapping("/polygon/erc721")
+class EphereProfilePicOnPolygonController(
+  private val ephereProfilePicOnPolygonService: EphereProfilePicOnPolygonService
 ) {
 
   @GetMapping("/{tokenId}")
   fun getById(@PathVariable tokenId: Int): ERC721Metadata {
-    return ephereProfilePicService.getMetadataFromTokenId(tokenId)
+    return ephereProfilePicOnPolygonService.getMetadataFromTokenId(tokenId)
   }
 
 }
